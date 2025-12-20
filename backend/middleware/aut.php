4272d29d5e032797196abc60c714112ef59ec6ac
yacine <bouregaayacine31@gmail.com>
2025-12-20 18:37:48 +0100

    last update adding some frontend files

--- backend/middleware/aut.php
+++ backend/middleware/aut.php
@@ -1,28 +1,33 @@
 <?php
- 
- 
-    //Middleware to check if user is authenticated
- function checkAuth(){
-    //check if session is started
-   if(session_status() == PHP_SESSION_NONE){
+
+// 1. Middleware to check if user is authenticated
+function checkAuth() {
+    if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
-   
-    if(!isset($_SESSION['is_logged_in']) || $_SESSION['is_logged_in'] !== true){
-        http_response_code(401);//Unauthorized
+
+    if (!isset($_SESSION['is_logged_in']) || $_SESSION['is_logged_in'] !== true) {
+        http_response_code(401); // Unauthorized
         echo json_encode([
-            "status"=>"error",
-            "message"=>"Unauthorized access. Please log in."
+            "status" => "error",
+            "message" => "Unauthorized access. Please log in."
         ]);
         exit;
     }
+}
 
-    if($_SESSION['role'] !=='admin'){
-        http_response_code(403);//forbiden access
+// 2. Middleware to check if user is Admin
+function checkAdmin() {
+    
+    checkAuth();
+
+
+    if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+        http_response_code(403); // Forbidden access
         echo json_encode([
-            "status"=>"error",
-            "message"=>"Forbidden access. Admins only." 
+            "status" => "error",
+            "message" => "Forbidden access. Admins only."
         ]);
         exit();
     }
- }
+}
